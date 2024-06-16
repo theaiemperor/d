c67ff208fbfcb600be7d480c2102fa0dbe5a19b2
Arman <119550818+theaiemperor@users.noreply.github.com>
2024-06-16 15:26:08 +0530

    created calculator

---
+++ code/code.py
@@ -0,0 +1,11 @@
+def calculate(a,b,c="+"):
+    if c == "-":
+        return a-b
+    elif c == "*":
+        return a*b
+    elif c == "/":
+        return a/b
+    else:
+        return a+b
+    
+
